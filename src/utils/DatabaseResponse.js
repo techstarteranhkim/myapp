c200089e597d939ad7eba6b957c885c3bc237559
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-02-02 14:15:53 +0100

    improved databaserepsonse

--- src/utils/DatabaseResponse.js
+++ src/utils/DatabaseResponse.js
@@ -3,6 +3,8 @@ import { faker } from "@faker-js/faker";
 
 const DatabaseResponse = {
   profile: {
+    username: faker.person.firstName("female"),
+    birthdate: Math.floor(Math.random() * (80 - 18 + 1)) + 18, //Generiert alter 18-80
     followerCount: faker.number.int(10000),
     followingCount: faker.number.int(100),
     postCount: faker.number.int(20),
